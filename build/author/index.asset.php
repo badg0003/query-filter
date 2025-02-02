fcac44029339e36b71e40e3c7168c6505cfd2bf9
Mike Badgley <mike@digiteam.ca>
2025-02-02 10:59:03 -0500

    Build files

--- build/author/index.asset.php
+++ build/author/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => 'a7b020f1b5519d304c59');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '9baf7de99e612bea462c');
