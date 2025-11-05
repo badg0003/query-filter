55a2cf182b272ea5c825047afce751323013764f
Mike Badgley <mike@digiteam.ca>
2025-11-04 21:09:14 -0500

    Add display type options for taxonomy filter block

--- build/taxonomy/view.asset.php
+++ build/taxonomy/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => 'e7ba522c0e73d74a56f1', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '2c36123f2683dfabab36', 'type' => 'module');
